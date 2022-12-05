6bbac493beeedcb3dc16ceebfb0ff48ffa296429
Nausher Rao <sherrao15@gmail.com>
2022-12-05 18:19:21 -0500

    Add game loop functionality and start work on snake

--- src/main.cpp
+++ src/main.cpp
@@ -21,18 +21,21 @@ void render(void);
 void onResize(GLint width, GLint height);
 void onMouseAction(GLint button, GLint action, GLint xMouse, GLint yMouse);
 void onMouseMove(GLint xMouse, GLint yMouse);
-void onKeyboardAction(unsigned char key, GLint xMouse, GLint yMouse);
+void onKeyPress(unsigned char key, GLint xMouse, GLint yMouse);
 void drawSnake(void);
 void drawApples(void);
 void drawBombs(void);
 void logConsole(char *functionName, char *message);
 
 // Constants
-const int WIDTH = 1280;
-const int HEIGHT = 720;
 const char *TITLE = "Snek - Version 1.0";
+const int FPS = 1000 / 60;
 
 // Variables
+int width = 1280;
+int height = 720;
+bool running = true;
+
 Player player;
 Bomb bombs[100];
 Apple apples[100];
@@ -40,9 +43,123 @@ int score;
 int bombCount;
 int appleCount;
 
-int main(int argc, char *argv[])
+void update(int value)
 {
-    init(argc, argv);
+    // Update the player's position.
+    player.x += player.xVelocity;
+    player.y += player.yVelocity;
+
+    glutPostRedisplay();
+    glutTimerFunc(FPS, update, 0);
+}
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    drawSnake();
+    glFlush();
+}
+
+/**
+ * @brief Called by OpenGL when the window is resized. Updates the viewport and project matrix.
+ */
+void onResize(GLint newWidth, GLint newHeight)
+{
+    char logMessage[200];
+    sprintf(logMessage, "Resizing window to new dimensions: (%d, %d)", newWidth, newHeight);
+    logConsole("onResize", logMessage);
+
+    glViewport(0, 0, newWidth, newHeight);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, GLdouble(newWidth), GLdouble(newHeight), 0.0);
+}
+
+/**
+ * @brief Called by OpenGL when the mouse has an action occur, such as a click or release.
+ */
+void onMouseAction(GLint button, GLint action, GLint xMouse, GLint yMouse)
+{
+    char logMessage[200];
+    sprintf(logMessage, "Mouse action occured at (%d, %d)", xMouse, yMouse);
+    logConsole("onMouseAction", logMessage);
+}
+
+/**
+ * @brief Called by OpenGL when the mouse is moved.
+ */
+void onMouseMove(GLint xMouse, GLint yMouse)
+{
+    char logMessage[200];
+    sprintf(logMessage, "Mouse moved to (%d, %d)", xMouse, yMouse);
+    logConsole("onMouseMove", logMessage);
+}
+
+/**
+ * @brief Called by OpenGL when a keyboard key is pressed.
+ */
+void onKeyPress(unsigned char key, GLint x, GLint y)
+{
+    char logMessage[200];
+    sprintf(logMessage, "Keyboard key pressed: %c", key);
+    logConsole("onKeyPress", logMessage);
+
+    // Escape key
+    if (key == 27)
+    {
+        exit(0);
+    }
+
+    if (key == 'w' || key == GLUT_KEY_UP)
+    {
+        player.yVelocity = 1;
+        player.xVelocity = 0;
+    }
+    if (key == 's' || key == GLUT_KEY_DOWN)
+    {
+        player.yVelocity = -1;
+        player.xVelocity = 0;
+    }
+    if (key == 'a' || key == GLUT_KEY_LEFT)
+    {
+        player.xVelocity = -1;
+        player.yVelocity = 0;
+    }
+    if (key == 'd' || key == GLUT_KEY_RIGHT)
+    {
+        player.xVelocity = 1;
+        player.yVelocity = 0;
+    }
+}
+
+void onSpecialKeyPress(int key, int a, int b)
+{
+    char logMessage[200];
+    sprintf(logMessage, "Special keyboard key pressed: %d", key);
+    logConsole("onSpecialKeyPress", logMessage);
+}
+
+void drawSnake()
+{
+    glColor3f(player.color[0], player.color[1], player.color[2]);
+    glBegin(GL_POLYGON);
+    glVertex2f(player.x, player.y);
+    glVertex2f(player.x + player.size, player.y);
+    glVertex2f(player.x + player.size, player.y + player.size);
+    glVertex2f(player.x, player.y + player.size);
+    glEnd();
+}
+
+void drawApples() {}
+
+void drawBombs() {}
+
+void logConsole(char *functionName, char *message)
+{
+    printf("[%s]: %s\n", functionName, message);
 }
 
 void init(int argc, char *argv[])
@@ -65,115 +182,24 @@ void init(int argc, char *argv[])
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowPosition(0, 0);
-    glutInitWindowSize(WIDTH, HEIGHT);
+    glutInitWindowSize(width, height);
     glutCreateWindow(TITLE);
 
     glutDisplayFunc(render);
     glutReshapeFunc(onResize);
     glutMouseFunc(onMouseAction);
     glutMotionFunc(onMouseMove);
-    glutKeyboardFunc(onKeyboardAction);
+    glutKeyboardFunc(onKeyPress);
+    glutSpecialFunc(onSpecialKeyPress);
+    glutTimerFunc(FPS, update, 0);
 
     glClearColor(0.6, 0.6, 0.6, 1.0);
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D(0.0, 300.0, 0.0, 300.0);
     glutMainLoop();
-
-    // point.push_back(make_pair(bodyX - 1, bodyY));
-    // point.push_back(make_pair(bodyX - 2, bodyY));
-
-    // srand(time(NULL)); // execute initialization procedure
-    // newFood();
-
-    // glutTimerFunc(200, update, 0);
 }
 
-void render()
+int main(int argc, char *argv[])
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    // Update the player's position.
-    player.x += player.xVelocity;
-    player.y += player.yVelocity;
-
-    glFlush();
-}
-
-/**
- * @brief Called by OpenGL when the window is resized. Updates the viewport and project matrix.
- */
-void onResize(GLint newWidth, GLint newHeight)
-{
-    char *logMessage;
-    sprintf(logMessage, "Resizing window to new dimensions: (%d, %d)", newWidth, newHeight);
-    logConsole("onResize", logMessage);
-
-    glViewport(0, 0, newWidth, newHeight);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0, GLdouble(newWidth), GLdouble(newHeight), 0.0);
-}
-
-/**
- * @brief Called by OpenGL when the mouse has an action occur, such as a click or release.
- */
-void onMouseAction(GLint button, GLint action, GLint xMouse, GLint yMouse)
-{
-    char *logMessage;
-    sprintf(logMessage, "Mouse action occured at (%d, %d)", xMouse, yMouse);
-    logConsole("onMouseAction", logMessage);
-}
-
-/**
- * @brief Called by OpenGL when the mouse is moved.
- */
-void onMouseMove(GLint xMouse, GLint yMouse)
-{
-    char *logMessage;
-    sprintf(logMessage, "Mouse moved to (%d, %d)", xMouse, yMouse);
-    logConsole("onMouseMove", logMessage);
-}
-
-/**
- * @brief Called by OpenGL when a keyboard key is pressed.
- */
-void onKeyboardAction(unsigned char key, GLint x, GLint y)
-{
-    char *logMessage;
-    sprintf(logMessage, "Keyboard key pressed: %c", key);
-    logConsole("onKeyboardAction", logMessage);
-
-    if (key == 'w')
-    {
-        player.yVelocity = 1;
-        player.xVelocity = 0;
-    }
-    if (key == 's')
-    {
-        player.yVelocity = -1;
-        player.xVelocity = 0;
-    }
-    if (key == 'a')
-    {
-        player.xVelocity = -1;
-        player.yVelocity = 0;
-    }
-    if (key == 'd')
-    {
-        player.xVelocity = 1;
-        player.yVelocity = 0;
-    }
-}
-
-void drawSnake() {}
-
-void drawApples() {}
-
-void drawBombs() {}
-
-void logConsole(char *functionName, char *message)
-{
-    printf("[%s]: %s\n", functionName, message);
+    init(argc, argv);
 }
